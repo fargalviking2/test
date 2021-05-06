3cdb1e8ec5685b1e3b4ee1e22b4faaad70fbd086
fargalviking2 <80988313+fargalviking2@users.noreply.github.com>
2021-05-06 19:25:19 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,4 +10,56 @@ $(document).ready(function () {
 		}
 		$(this).toggleClass('active').next().slideToggle(300);
 	});
+	$('.slider').slick({
+		arrows: false,
+		dots: true,
+		adaptiveHeight: true,
+		slidesToShow: 4,
+		responsive: [
+			{
+				breakpoint: 1240,
+				settings: {
+					slidesToShow: 3,
+				}
+			},
+			{
+				breakpoint: 980,
+				settings: {
+					slidesToShow: 2,
+				}
+			},
+			{
+				breakpoint: 680,
+				settings: {
+					slidesToShow: 1,
+				}
+			},
+		]
+	});
+	$('.slider2').slick({
+		arrows: true,
+		dots: false,
+		adaptiveHeight: true,
+		slidesToShow: 4,
+		responsive: [
+			{
+				breakpoint: 1240,
+				settings: {
+					slidesToShow: 3,
+				}
+			},
+			{
+				breakpoint: 980,
+				settings: {
+					slidesToShow: 2,
+				}
+			},
+			{
+				breakpoint: 680,
+				settings: {
+					slidesToShow: 1,
+				}
+			},
+		]
+	});
 });
