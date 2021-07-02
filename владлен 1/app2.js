ee7aef2e7e3406adcc06b8066012e2650adddcb2
fargalviking2 <80988313+fargalviking2@users.noreply.github.com>
2021-07-02 20:36:40 +0300

    Add files via upload

---
+++ владлен 1/app2.js
@@ -0,0 +1,18 @@
+Vue.createApp({
+	data: () => ({
+		myHtml: '<h1>Vue 3 App</h1>',
+		title: 'fuck',
+		person: {
+			firstName: 'Vladilen',
+			lastName: 'Minin',
+			age: 27,
+		},
+		items: [1, 2, 3, 4, 5, 6]
+	}),
+	computed: {
+		evenItems() {
+			return this.items.filter(i => i % 2 === 0)
+		}
+	},
+	methods: {}
+}).mount('#app');
