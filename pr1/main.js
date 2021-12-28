21d3e346fc14869151e82501d62e1134b2713ef4
fargalviking2 <80988313+fargalviking2@users.noreply.github.com>
2021-12-29 00:54:52 +0300

    Add files via upload

--- pr1/main.js
+++ pr1/main.js
@@ -1,6 +1,6 @@
 new Swiper('.image-slider', {
 	speed: 400,
-	spaceBetween: 200,
+	spaceBetween: 300,
 	loop: true, autoHeight: true,
 	navigation: {
 		nextEl: '.swiper-button-next1',
@@ -10,10 +10,57 @@ new Swiper('.image-slider', {
 new Swiper('.image-slider2', {
 	speed: 400,
 	spaceBetween: 200,
-	loop: true, 
+	loop: true,
 	navigation: {
 		nextEl: '.swiper-button-next2',
 		prevEl: '.swiper-button-prev2',
 	},
 
+});
+new Swiper('.image-slider3', {
+	speed: 400,
+	spaceBetween: 00,
+	slidesPerView: 2,
+	loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next3',
+		prevEl: '.swiper-button-prev3',
+	},
+
+});
+linkMain.addEventListener("click", function (e) {
+	document.getElementById("main").scrollIntoView({
+		behavior: 'smooth',
+		block: 'start'
+	});
+});
+linkAboutMe.addEventListener("click", function (e) {
+	document.getElementById("about-me").scrollIntoView({
+		behavior: 'smooth',
+		block: 'start'
+	});
+});
+linkAboutBook.addEventListener("click", function (e) {
+	document.getElementById("about-book").scrollIntoView({
+		behavior: 'smooth',
+		block: 'start'
+	});
+});
+linkBookContent.addEventListener("click", function (e) {
+	document.getElementById("book-content").scrollIntoView({
+		behavior: 'smooth',
+		block: 'start'
+	});
+});
+linkReviews.addEventListener("click", function (e) {
+	document.getElementById("reviews").scrollIntoView({
+		behavior: 'smooth',
+		block: 'start'
+	});
+});
+linkBuy.addEventListener("click", function (e) {
+	document.getElementById("buy").scrollIntoView({
+		behavior: 'smooth',
+		block: 'start'
+	});
 });
