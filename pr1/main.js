684e8f3cf82fb60419f4378276eb9c0d8308a068
fargalviking2 <80988313+fargalviking2@users.noreply.github.com>
2021-12-28 03:34:23 +0300

    Add files via upload

---
+++ pr1/main.js
@@ -0,0 +1,18 @@
+new Swiper('.image-slider', {
+	speed: 400,
+	spaceBetween: 200,
+	loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next1',
+		prevEl: '.swiper-button-prev1',
+	},
+});
+new Swiper('.image-slider2', {
+	speed: 400,
+	spaceBetween: 200,
+	loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next2',
+		prevEl: '.swiper-button-prev2',
+	},
+});
---
+++ pr1/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	mode: 'jit',
+	content: [
+		"./*.html",
+	],
+	darkMode: "media", // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+}
